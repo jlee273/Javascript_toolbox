c5d96f8c23b82b9db13f3488954a1fbf382ec379
jlee273 <54851500+jlee273@users.noreply.github.com>
2020-10-27 10:58:06 +0900

    chnage

--- main.js
+++ main.js
@@ -1,32 +1,15 @@
-public class MainApp
-{
-    public static void main(String[] args)
-    {
-        String message = "Lorem ipsum dolor sit amet, \nconsectetur adipisicing elit. Nobis, nihil. \nUllam magnam nam repellat recusandae?";
-        slowPrint(message, 30);
-    }
 
-    /**
-     * Function to print each character in a string with a delay (a "typewriter" effect)
-     * @param message The string to print
-     * @param millisPerChar Milliseconds to take to print each character
-     */
-    public static void slowPrint(String message, long millisPerChar)
-    {
-        for (int i = 0; i < message.length(); i++)
-        {
-            System.out.print(message.charAt(i));
 
-            try
-            {
-                Thread.sleep(millisPerChar);
-            }
-            catch (InterruptedException e)
-            {
-                e.printStackTrace();
-            }
-        }
-    }
+var pw = "universe";
+
+var user_pw = prompt("Enter Password","");
+
+if (pw == user_pw){
+    document.write("Welcome");
+
+} else {
+    alert("Fail")
+    location.reload();
 }
 
 
